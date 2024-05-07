943c0f5a6d6e37c6ba89d1ec0d553c47021d6b3c
trulyaman25 <trulyaman25@gmail.com>
2024-05-08 04:06:22 +0530

    Partially Completed Homepage

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import logo from '../assets/logos/logoTransparent.png';
+import logo from '../assets/logos/logoWithBackground.png';
 import navbarMenu from '../assets/icons/navbarMenu.png';
 
 function Header() {
@@ -34,9 +34,9 @@ function Header() {
                     <nav className='flex justify-between items-center min-h-32'>
                         <div className='px-2 py-6 xl:px-10 xl:py-10 flex justify-center items-center'>
                             <a href="/">
-                                <h1 className='hidden md:block font-inter text-4xl font-black text-blackbean'>MOTOSPORTS</h1>
-                                <h1 className='md:hidden font-inter text-3xl font-black text-blackbean'>MOTO</h1>
-                                <h1 className='md:hidden font-inter text-3xl font-black text-blackbean'>SPORTS</h1>
+                                <h1 className='hidden md:block font-brushKing text-2xl font-black text-blackbean'>MOTOSPORTS</h1>
+                                <h1 className='md:hidden font-brushKing text-3xl font-black text-blackbean'>MOTO</h1>
+                                <h1 className='mt-3 md:hidden md:mt-0 font-brushKing text-3xl font-black text-blackbean'>SPORTS</h1>
                             </a>
                         </div>
 
